ac236d45b188bafb1aef415b62b2b13070246feb
Amit <Amitmaa12345@gmail.com>
2023-11-24 11:58:37 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,21 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, {useState} from 'react'
+import ItemList from './itemList';
+import Display from './display'
+import CSSS from '../styles/App.css'
 
+function App() {
+    const [list, setList] = useState([])
+
+  return (
+    <div>
+      <h1 style={{
+        width: "25%",
+        margin: "auto"
+    }}>Our Menu</h1>
+      <ItemList setList={setList}/>
+      <Display list={list} />
+    </div>
+  )
+}
+
+export default App
